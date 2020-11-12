64e41f389a90559732d6c0e6cea87519fd90530d
Ale <alessandrochb@gmail.com>
2020-11-12 21:28:54 +0100

    add messaggi dinamicamente con 'focus' al click sui rispettivi <LI>

--- assets/main.js
+++ assets/main.js
@@ -12,86 +12,94 @@ var app = new Vue ({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent-messages'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di dargli da mangiare',
-                        status: 'sent'
+                        status: 'sent-messages'
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'recived-messages'
                     }
                 ]
             },
             {
                 name: 'Contatto2',
                 photo: 'img/third.jpg',
-                visible: true,
+                visible: false,
                 messages: [
                     {
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent-messages'
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'recived-messages'
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'received'
+                        status: 'sent-messages'
                     }
                 ]
             },
             {
                 name: 'Contatto3',
                 photo: 'img/fourth.jpg',
-                visible: true,
+                visible: false,
                 messages: [
                     {
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'recived-messages'
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent-messages'
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
-                        status: 'received'
+                        status: 'recived-messages'
                     }
                 ]
             },
             {
                 name: 'Contatto4',
                 photo: 'img/fifth.jpg',
-                visible: true,
+                visible: false,
                 messages: [
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent-messages'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'recived-messages'
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
-                        status: 'received'
+                        status: 'sent-messages'
                     }
                 ]
             },
         ],
-    }
+        active: 0,
+    },
+    methods:{
+        // funzione che al click deve far cambiare lo stato di active e prendere il valore dell'index dell'elemento cliccato
+        changeChat(index){
+            this.active = index;
+            // console.log(index);
+        }
+    },
 });
